eda2e08e4c9caeb9c5b12f3a58fb03305ea749e8
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-08-31 16:21:13 -0600

    Agregar archivos de proyecto.

---
+++ BusquedaRecursiva/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace BusquedaRecursiva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvFolders = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFolders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFolders
+            // 
+            this.dgvFolders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFolders.Location = new System.Drawing.Point(12, 12);
+            this.dgvFolders.Name = "dgvFolders";
+            this.dgvFolders.RowHeadersWidth = 51;
+            this.dgvFolders.RowTemplate.Height = 24;
+            this.dgvFolders.Size = new System.Drawing.Size(1154, 590);
+            this.dgvFolders.TabIndex = 0;
+            this.dgvFolders.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1178, 614);
+            this.Controls.Add(this.dgvFolders);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFolders)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFolders;
+    }
+}
+
---
+++ BusquedaRecursiva/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BusquedaRecursiva
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dgvFolders.Columns.Add("Files", "Folders and files");
+            dgvFolders.Columns[0].Width = 1000;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                MessageBox.Show("Selected folder: " + dialog.SelectedPath, "Folder Selection",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                FileRevision(dialog.SelectedPath);
+                MessageBox.Show("If nothing is displayed in the 'DataGridView', this might be due to an inaccessible folder.");
+            }
+        }
+
+        public void FileRevision(string mainFolder)
+        {
+            if (string.IsNullOrEmpty(mainFolder))
+            {
+                MessageBox.Show("Main folder is empty");
+                return;
+            }
+
+            foreach (string subFolder in Directory.GetDirectories(mainFolder))
+            {
+                dgvFolders.Rows.Add("> " + subFolder.PadRight(15));
+                Console.WriteLine("\n " + subFolder);
+
+                foreach (string file in Directory.GetFiles(subFolder))
+                {
+                    dgvFolders.Rows.Add(file);
+                    Console.WriteLine(file);
+                }
+
+                FileRevision(subFolder);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
